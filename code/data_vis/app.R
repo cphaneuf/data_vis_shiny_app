513dd8f59333a4f5961dd922d0046b9a7e776800
cphaneuf <cphaneuf@umich.edu>
2021-12-28 00:25:34 -0500

    shiny app templating changes.

--- code/data_vis/app.R
+++ code/data_vis/app.R
@@ -8,13 +8,34 @@
 # Run Shiny web application by clicking the 'Run App' button above.
 # For more information: http://shiny.rstudio.com/
 
+# Load necessary packages
 library(shiny)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
+# Read in data
+setwd("/Users/camillephaneuf/Desktop/ANDL/HCPD/data_vis_shiny_app/code/data_vis")
+data <- read.csv('../../data/data.csv')
 
+# Define UI for application that does XYZ
+ui <- fluidPage(
+    
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Exploratory Data Analysis"),
+    h3('Welcome to this Shiny App created by cphaneuf!'),
+    
+    # Display user name greeting
+    textInput("user_name", "What is your name?", "JANE DOE"),
+    textOutput("user_name_greeting"),
+    h3('\n'),
+    
+    # Display data name greeting
+    textInput("data_name", "What is the name of the .csv data file that you will be visualizing today? This .csv file should be stored in data_vis_shiny_app/data.", "EXAMPLE.csv"),
+    textOutput("data_name_greeting"),
+    
+    # Visualize continuous numeric variables
+    h3('Visualize Continuous Numeric Variables'),
+    
+    # Interactive histograms
+    h4('Histograms'),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
@@ -30,12 +51,32 @@ ui <- fluidPage(
         mainPanel(
            plotOutput("distPlot")
         )
-    )
+    ),
+    
+    # Interactive scatterplots
+    h4('Scatterplots'),
+    
+    selectInput('x_var', 'Select x', names(data), 'age'),
+    selectInput('y_var', 'Select y', names(data), 'task_1'),
+    #numericInput('something', 'Somthing', 3),
+    #plotOutput("func")
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
+#server <- function(input, output, session) {
+    
+    # Define user name greeting
+    output$user_name_greeting <- renderText({
+        paste0("Welcome to your simple, exploratory data analysis, ", input$user_name, "!")
+    })
+    
+    # Define data name greeting
+    output$data_name_greeting <- renderText({
+        #data <- read.csv('../../data/data.csv')
+        paste0("Thanks! Let's get started with ", input$data_name, " then.")
+    })
+    
     output$distPlot <- renderPlot({
         # generate bins based on input$bins from ui.R
         x    <- faithful[, 2]
@@ -43,6 +84,10 @@ server <- function(input, output) {
 
         # draw the histogram with the specified number of bins
         hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    })
+    
+    output$func <- renderPlot({
+        plot(x_var, y_var)
     })
 }
 
